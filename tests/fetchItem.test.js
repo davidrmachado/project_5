a0bcad101435fa0fd920e5d9463251177ddfd18d
David-Neto <davidrmachado@gmail.com>
2022-06-08 03:25:04 -0300

    Corrigido erros no requisito 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -11,18 +11,18 @@ describe('2 - Teste a função fetchItem', () => {
   })
 
   it('Verifica se, ao executar a função fetchItem com o argumento "MLB1615760527", a função fetch é chamada', async () => {
-    await fetchProducts('MLB1615760527');
+    await fetchItem('MLB1615760527');
     expect(fetch).toHaveBeenCalled();
   })
 
   it('Verifica se, ao chamar a função fetchItem com o argumento "MLB1615760527", a função fetch utiliza o endpoint "https://api.mercadolibre.com/items/MLB1615760527"', async () => {
-    await fetchProducts('MLB1615760527');
+    await fetchItem('MLB1615760527');
     const expected = 'https://api.mercadolibre.com/items/MLB1615760527';
     expect(fetch).toHaveBeenCalledWith(expected);
   })
 
   it('Verifica se o retorno da função fetchItem com o argumento "MLB1615760527" é uma estrutura de dados igual ao objeto item', async () => {
-    const actual = await fetchProducts('item');
+    const actual = await fetchItem('item');
     expect(actual).toEqual(item);
   })
 
